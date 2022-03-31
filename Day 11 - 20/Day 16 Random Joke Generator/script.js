d37e692ebc0f3abf2e9ac800b6d226a47fe7e9c5
Yash solanki <47449493+yashsolanki22@users.noreply.github.com>
2022-03-31 20:29:24 +0530

    Project 11-20

--- Day 11 - 20/Day 16 Random Joke Generator/script.js
+++ Day 11 - 20/Day 16 Random Joke Generator/script.js
@@ -6,38 +6,33 @@ const api = "http://api.icndb.com/jokes/random";
 
 btn.addEventListener("click", getJoke);
 
+// When we deal with API data we want our function to be work seamless thus we would make function async and it will return a promise.
 async function getJoke() {
-    let response = await fetch(api); // fetch the api before moving forward
+    // fetch the api before moving forward
+    let response = await fetch(api); 
     // console.log(response); Here we can see the response 
-    let data = await response.json(); // In order to see it in human readable form we need to convert that data into a JSON format
-    console.log(data);  // Here we could see the joke being generated in our console
+    // In order to see it in human readable form we need to convert that data into a JSON format.
+    let data = await response.json(); 
+    
+    // Here we could see the joke being generated in our console
+    console.log(data);
+    // let's get the joke and render it to joke variable 
     joke.innerHTML = data.value.joke;
+    // an extra feature we have added :) let's get the joke id as well for that particular joke 
     jokeId.innerHTML = `Joke ID: ${data.value.id}`;
+    
+    // we also want to get the category for that joke but the joke we get sometime doesn't have any catogory.
+    // So we would provide a condition, If the category property has no value which means the length is 0 we wouild just display "lame" as category.
     if(data.value.categories.length == [0]) {
         const noJoke = "lame";
         jokeCategory.innerHTML = `Joke Category: ${noJoke}`
     }
+    // if the joke has any category we would display that category value only. 
     else {
     jokeCategory.innerHTML = `Joke Category: ${data.value.categories}`;
     // categories can be used to get the category of the joke
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
