5d7da034aa86e40801efe865d611c988c73053fa
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-03-31 19:40:28 +0430

    Projects 11 - 20

--- Day 11 - 20/Day 16 Random Joke Generator/script.js
+++ Day 11 - 20/Day 16 Random Joke Generator/script.js
@@ -6,7 +6,7 @@ const api = "http://api.icndb.com/jokes/random";
 
 btn.addEventListener("click", getJoke);
 
-// When we deal with API data we want our function to be work seamless thus we would make function async and it will return a promise.
+// When we deal with API data we want our function to work seamlessly, thus we would make function async and it will return a promise.
 async function getJoke() {
     // fetch the api before moving forward
     let response = await fetch(api); 
@@ -18,11 +18,11 @@ async function getJoke() {
     console.log(data);
     // let's get the joke and render it to joke variable 
     joke.innerHTML = data.value.joke;
-    // an extra feature we have added :) let's get the joke id as well for that particular joke 
+    // an extra feature we have added :) let's get the joke id as well for that particular joke which can be seen in the api after you console the data in line 18
     jokeId.innerHTML = `Joke ID: ${data.value.id}`;
     
-    // we also want to get the category for that joke but the joke we get sometime doesn't have any catogory.
-    // So we would provide a condition, If the category property has no value which means the length is 0 we wouild just display "lame" as category.
+    // we also want to get the category for that joke but the joke we get sometimes doesn't have any catogory.
+    // So we would provide a condition, If the category property has no value which means the length is 0 we would just display "lame" as category.
     if(data.value.categories.length == [0]) {
         const noJoke = "lame";
         jokeCategory.innerHTML = `Joke Category: ${noJoke}`
@@ -35,7 +35,7 @@ async function getJoke() {
 }
 
 
-
+ // Here's how this project would work if we didn't use asynchronous functions
 /*
 btn.addEventListener("click" , () => { //// whenever you use the fetch api a promise is returned
  fetch(api).then(response => {
