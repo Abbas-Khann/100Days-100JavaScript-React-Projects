57e5d4284fba1d3b3e0e2ff343d2db7aa8d0262c
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-03-31 15:39:56 +0430

    Scrimba Javascript Projects

---
+++ Scrimba Javascript/Emoji App/index.js
@@ -0,0 +1,45 @@
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+
+function renderEmojis() {
+    const emojiContainer = document.getElementById("emoji-container")
+    emojiContainer.innerHTML = ""
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
+}
+
+renderEmojis()
+
+const pushBtn = document.getElementById("push-btn")
+pushBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+const unshiftBtn = document.getElementById("unshift-btn")
+unshiftBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+const popBtn = document.getElementById("pop-btn")
+popBtn.addEventListener("click", function(){
+    myEmojis.pop()
+    renderEmojis()
+})
+
+const shiftBtn = document.getElementById("shift-btn")
+shiftBtn.addEventListener("click", function(){
+    myEmojis.shift()
+    renderEmojis()
+})
