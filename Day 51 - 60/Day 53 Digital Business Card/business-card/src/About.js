e758ff8a5776adb773cf98b366d68f9b81e0aa90
Abbas Khan <78753275+Abbas-Khann@users.noreply.github.com>
2022-04-27 11:20:28 +0430

    Projects 51 - 60

---
+++ Day 51 - 60/Day 53 Digital Business Card/business-card/src/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+    return(
+        <div className="about-container">
+            
+            <h2>About Me</h2>
+            <p className="about-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut interdum purus, elementum tincidunt arcu. Donec at nulla in neque facilisis laoreet nec ac risus. Nulla finibus sapien ac augue facilisis.</p>
+
+        </div>
+    )
+}
+
+export default About
---
+++ Day 51 - 60/Day 53 Digital Business Card/business-card/src/Interests.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Interests() {
+    return(
+        <div className="interests-container">
+            
+            <h2 className="interests-heading">Interests</h2>
+            <p className="interests-para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut interdum purus, elementum tincidunt arcu. Donec at nulla in neque facilisis laoreet nec ac risus. Nulla finibus sapien ac augue facilisis.</p>
+
+        </div>
+    )
+}
+
+export default Interests
